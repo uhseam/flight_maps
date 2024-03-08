d84e6cd9f529916286031ccaebfafee1d17b7686
uhseam <aseemsingh12@gmail.com>
2024-03-07 21:29:17 -0500

    views and settings

--- flight_maps/settings.py
+++ flight_maps/settings.py
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DJANGO_SETTINGS_MODULE = 'flight-maps.settings'
