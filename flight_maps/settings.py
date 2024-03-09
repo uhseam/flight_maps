979465c546051bebd8548526ad1555868cb7b086
uhseam <aseemsingh12@gmail.com>
2024-03-09 12:20:36 -0500

    added flight_maps_app files and created pytest configuration

--- flight_maps/settings.py
+++ flight_maps/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flight_maps_app.apps.FlightMapsAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DJANGO_SETTINGS_MODULE = 'flight-maps.settings'
