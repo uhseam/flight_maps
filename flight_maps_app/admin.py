d997a0c4cf96a6b125f46f5b6ff6c66db4f7c157
kwakinator-quack <127004926+kwakinator-quack@users.noreply.github.com>
2024-04-21 01:45:48 -0400

    Registering models into admin

--- flight_maps_app/admin.py
+++ flight_maps_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from models import *
+
+admin.site.register(Flight)
+admin.site.register(Reservation)
+admin.site.register(CustomUser)
+admin.site.register(UserProfile)
+admin.site.register(FlightReservation)
 # Register your models here.
