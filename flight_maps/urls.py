922a8c57bb967229407f0b1b3cc4ae3de7af29d8
hoanlee0802 <43596606+hoanlee0802@users.noreply.github.com>
2024-04-21 21:21:53 -0400

    Update urls.py

--- flight_maps/urls.py
+++ flight_maps/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import booking_url_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    path('get-booking-url/', booking_url_view, name='get-booking-url'),
 ]
