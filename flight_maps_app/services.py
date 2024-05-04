c5376ca57db3a0387a4b759bcf44b018f4971548
vcr27 <162660409+vcr27@users.noreply.github.com>
2024-05-03 23:03:22 -0400

    Update services.py

--- flight_maps_app/services.py
+++ flight_maps_app/services.py
@@ -26,3 +26,25 @@ def get_booking_url(search_hash, dest, id, orig, search_id):
         return response.json()
     else:
         raise Exception(f"Error: {response.status_code}, {response.text}")
+
+
+
+def get_us_airport():
+    api_key = os.getenv('RAPIDAPI_KEY')
+    if not api_key:
+        raise Exception("API key not found. Please set the RAPIDAPI_KEY environment variable.")
+
+    url = "https://airport-metadata.p.rapidapi.com/v1/airports/by-country/US"
+
+    headers = {
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": "airport-metadata.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Error: {response.status_code}, {response.text}")
+
